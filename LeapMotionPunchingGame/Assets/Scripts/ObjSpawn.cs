5d700606012de453f4da59519ac766120c0d3b86
xakazukinx <xakazukinx.goo@gmail.com>
2019-04-07 00:50:47 +0900

    [add]パンチするよ

---
+++ LeapMotionPunchingGame/Assets/Scripts/ObjSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjSpawn : MonoBehaviour
+{
+
+    [SerializeField] private Vector2 randomRange_X;
+    [SerializeField] private float spawnZ;
+    [SerializeField] private float spawnY;
+    [SerializeField] private GameObject spawnObject;
+    [SerializeField] private float spawnTime;
+    [SerializeField] private float destroyTime;
+
+    private void Start()
+    {
+        StartCoroutine(spawnObj());
+    }
+
+    private IEnumerator spawnObj()
+    {
+        while (true)
+        {
+            var spawn = Instantiate(spawnObject,
+                new Vector3(Random.Range(randomRange_X.x, randomRange_X.y), spawnY, spawnZ), Quaternion.identity);
+            Rigidbody spawnRigid = spawn.GetComponent<Rigidbody>();
+            spawnRigid.AddForce(Vector3.up,ForceMode.Impulse);
+            Destroy(spawn,destroyTime);
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
---
+++ LeapMotionPunchingGame/Assets/Scripts/Punch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+
+    [SerializeField] private float impulsWeight = 3.0f;
+    [SerializeField] private string targetObjectTag;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == targetObjectTag)
+        {
+            var rigid = other.gameObject.GetComponent<Rigidbody>();
+            var impulse = (rigid.position - transform.position).normalized * impulsWeight;
+            rigid.AddForce(impulse,ForceMode.Impulse);
+            Debug.Log("Hit!!!");
+        }
+    }
+}
